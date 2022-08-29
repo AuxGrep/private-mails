c56f437c606bf103587ccca9ac15310404eea1d9
AuxGrep <103135612+AuxGrep@users.noreply.github.com>
2022-08-29 21:03:44 +0300

    Update telegram.py

--- telegram.py
+++ telegram.py
@@ -1,18 +1,18 @@
 import subprocess
 import time
-#Sending email backups to telegram channel
-#what you need is telegram API + Bot chat id
-#There is alot of ways to find you bot chat id just learn it
-#THIS IS ONLY FOR EDUCATIONAL PURPOSE
-#HANG OUT !! AND ENJOY @hezron-Auxgrep
-
-#PLEASE ADD YOUR BOT TOKEN PLEASE > LINE NO: 16
+#				!!< REQUIREMENTS >!!
+# 1. You need telegram bot > use telegram botfather to build free telegramBot
+# 2. Copy telegram bot token and save it in your computer, we ll use it at a future
+# 3. use any technique to get your bot chat id and save it in your computer
+# 4. add your BOT to the channel and wait for inbox email backups from this script.
+#                           @Enjoy >> made by Hezron
+#				< Twitter AuxGrep > 
 
 def private():
 
 	cmd = "zip -r inbox.zip Inbox"
 	subprocess.call('xterm -T Telegram -e ' + cmd ,shell=True)
 	bot_id = input("Enter Bot chat_Id: ")
-	API = "https://api.telegram.org/bot5143646025:AAHysVZ4A2JaO9eQYpoWzSXHT-MZyCTkz3o/sendDocument?chat_id="
+	bot_token = input("Enter bot tokken: ")
+	API = "" + bot_token + "/sendDocument?chat_id="
 	subprocess.call('curl -F document=@inbox.zip ' + API + bot_id, shell=True)
-
